f53643640e17958e503271ab5d6b4ebfa603ab9e
yurubahmed <yurubiftin2002@gmail.com>
2023-02-09 19:57:14 -0500

    commit tree 2369b1c230a633d4d6032f5523bc82001d06a65e

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    attr_reader :name, :breed
+
+    def initialize(name ,breed ="Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    
+end
+
+fido = Dog.new("Fido" ,"pug")
+
